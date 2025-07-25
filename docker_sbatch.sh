872dea47cac7800fb0f3cbc26d05b688276cdd40
Siqi Ouyang <souyang@nvidia.com>
2025-07-25 13:53:52 -0700

    add yamls

--- docker_sbatch.sh
+++ docker_sbatch.sh
@@ -1,4 +1,4 @@
-ROOT=/lustre/fs11/portfolios/convai/users/souyang
+ROOT=/lustre/fsw/portfolios/llmservice/users/souyang
 
 CONTAINER_PATH=$ROOT/images/nemo_rl_npy.sqsh
 CODE_DIR=$ROOT/code
@@ -9,26 +9,26 @@ HF_CACHE_DIR=$ROOT/.cache/huggingface
 NUM_ACTOR_NODES=3  # Total nodes requested (head is colocated on ray-worker-0)
 
 CONFIG_NAME=$1
-N=3
+N=2
 
-HF_TOKEN=$(cat /home/souyang/.keys/hf_token)
-WANDB_API_KEY=$(cat /home/souyang/.keys/wandb_api_key)
+HF_TOKEN=$(cat $ROOT/.keys/hf_token)
+WANDB_API_KEY=$(cat $ROOT/.keys/wandb_api_key)
 
 JOB_NAME="${CONFIG_NAME}"
 
 for i in $(seq 1 ${N}); do
     # COMMAND="NRL_VLLM_USE_V1=0 PYTHONPATH=/code/RL:$PYTHONPATH uv run ./examples/run_grpo_infinisst.py --config ${CONFIG_NAME}" \
     COMMAND="TOKENIZERS_PARALLELISM=false PYTHONPATH=/code/RL:$PYTHONPATH uv run ./examples/run_grpo_infinisst.py --config /code/RL/examples/configs/${CONFIG_NAME}.yaml" \
-    MOUNTS="/lustre/fs11:/lustre/fs11,${CODE_DIR}:/code,${CKPTS_DIR}:/ckpts,${DATA_DIR}:/data" \
+    MOUNTS="/lustre/fsw:/lustre/fsw,${CODE_DIR}:/code,${CKPTS_DIR}:/ckpts,${DATA_DIR}:/data" \
     CONTAINER=${CONTAINER_PATH} \
     HF_TOKEN=${HF_TOKEN} \
     HF_DATASETS_CACHE=$HF_CACHE_DIR \
     WANDB_API_KEY=${WANDB_API_KEY} \
     sbatch \
         --nodes=${NUM_ACTOR_NODES} \
-        --account=convai_convaird_nemo-speech \
+        --account=llmservice_nemo_reasoning \
         --job-name=${JOB_NAME} \
-        --partition=batch_block1,batch_block3,batch_block4 \
+        --partition=batch \
         --dependency=singleton \
         --time=4:0:0 \
         --mail-type=ALL \
